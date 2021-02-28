3d9f4651899a2970ef8044eb4e430aa9a62c26ce
askprateek <askpk123@gmail.com>
2021-02-28 17:01:36 +0530

    [FIXED] timeago issue.

--- turbohire/turbohire.js
+++ turbohire/turbohire.js
@@ -23,12 +23,6 @@ var facets = {
 
 function renderCards(jobs) {
   $("#jobs>.grid-22").empty();
-  //   $("#w-dropdown-list-8").empty();
-  //   $("#w-dropdown-list-9").empty();
-  //   $("#w-dropdown-list-7").empty();
-  var departments = [];
-  var companies = [];
-  var fields = [];
 
   jobs.forEach(function (job) {
     $("#jobs>.grid-22").append(
@@ -61,83 +55,12 @@ function renderCards(jobs) {
          </div>
       </div>`)
     );
-    //alert(job.JobId)
-    var date1 = new Date(job.UpdatedDate);
-    var date2 = new Date();
-    //var Difference_In_Time = date2.getTime() - date1.getTime();
-    //var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
-    seconds = Math.floor((date2 - date1) / 1000);
-    minutes = Math.floor(seconds / 60);
-    hours = Math.floor(minutes / 60);
-    days = Math.floor(hours / 24);
-    weeks = Math.floor(days / 7);
-    months = Math.floor(weeks / 4);
+    var updatedAt = new Date(job.UpdatedDate);
+    var momentAgo = timeago().format(updatedAt);
 
-    weeks = weeks - months * 4;
-    days = days - months * 4 * 7 - weeks * 7;
-    hours = hours - months * 4 * 7 * 24 - weeks * 7 * 24 - days * 24;
-    minutes =
-      minutes -
-      months * 4 * 7 * 24 * 60 -
-      weeks * 7 * 24 * 60 -
-      days * 24 * 60 -
-      hours * 60;
-    seconds =
-      seconds -
-      months * 4 * 7 * 24 * 60 * 60 -
-      weeks * 7 * 24 * 60 * 60 -
-      days * 24 * 60 * 60 -
-      hours * 60 * 60 -
-      minutes * 60;
-
-    $(".text-block-" + job.JobId).html(
-      (months != 0 ? months + " Month " : "") +
-        (months == 0 && weeks != 0 ? weeks + " Week " : "") +
-        (weeks == 0 && days != 0 ? days + " Day " : "") +
-        (days == 0 && hours != 0 ? hours + " hrs " : "") +
-        (hours == 0 && minutes != 0 ? minutes + " Min " : "") +
-        " Ago"
-    );
-
-    //alert("Time until new year:\nDays: " + days + " Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds);
-    //    document.getElementsByClassName("text-block-23")[0].innerHTML = "Ago" + Difference_In_Days;
-
-    if (!departments.includes(job.Department)) {
-      departments.push(job.Department);
-    }
-
-    if (!companies.includes(job.CompanyName)) {
-      companies.push(job.CompanyName);
-    }
-    if (!fields.includes(job.JobTitle)) {
-      fields.push(job.JobTitle);
-    }
+    $(".text-block-" + job.JobId).html(momentAgo);
   });
-
-  //   departments.forEach(function (item) {
-  //     $("#w-dropdown-list-8").append(
-  //       $(
-  //         `<a onClick="renderDepartmentCards('${item}');" class="dropdown-link-4 w-dropdown-link" tabindex="0">${item}</a>`
-  //       )
-  //     );
-  //   });
-
-  //   companies.forEach(function (item) {
-  //     $("#w-dropdown-list-9").append(
-  //       $(
-  //         `<a onClick="renderCompanyCards('${item}');" class="dropdown-link-4 w-dropdown-link" tabindex="0">${item}</a>`
-  //       )
-  //     );
-  //   });
-
-  //   fields.forEach(function (item) {
-  //     $("#w-dropdown-list-7").append(
-  //       $(
-  //         `<a onClick="renderFieldCards('${item}');" class="dropdown-link-4 w-dropdown-link" tabindex="0">${item}</a>`
-  //       )
-  //     );
-  //   });
 }
 
 function setFacets(data) {
