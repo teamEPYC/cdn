ea5d0aa42d8692aa3cd1455a1b38945e4992c00c
Keshav Sharma <keshav@epyc.in>
2023-04-10 17:18:22 +0530

    SwiperJS Instagram stories slider file upload

---
+++ seedtoscale/SwiperJS Instagram Stories Slider/.eslintrc.js
@@ -0,0 +1,44 @@
+const rules = {
+  'import/no-extraneous-dependencies': 'off',
+  'no-param-reassign': ['off'],
+  'import/extensions': 'off',
+  'react/prop-types': 'off',
+};
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['react'],
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    ...rules,
+  },
+  overrides: [
+    {
+      extends: [
+        'plugin:react/recommended',
+        'airbnb-base',
+        'plugin:prettier/recommended',
+      ],
+      files: ['**/*.jsx', 'src/react/*.js'],
+      plugins: ['react'],
+      rules,
+    },
+    {
+      files: ['**/*.vue'],
+      extends: ['plugin:vue/vue3-recommended', 'prettier'],
+      rules: {
+        ...rules,
+      },
+    },
+  ],
+};
