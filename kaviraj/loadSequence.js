192f4b07997aea563ed118997f0a717b12cf2bda
alevikx <131615202+alevikx@users.noreply.github.com>
2025-06-05 00:00:54 +0530

    Update and rename homepage.js to loadSequence.js

---
+++ kaviraj/loadSequence.js
@@ -0,0 +1,43 @@
+const heroTimeline = gsap.timeline();
+
+const heading1 = new SplitText(".heading:nth-child(1)", { type: "chars" });
+const heading2 = new SplitText(".heading:nth-child(2)", { type: "chars" });
+const heading3 = new SplitText(".heading:nth-child(3)", { type: "chars" });
+const subtext = SplitText.create(".hero-subtext", { type: "chars" });
+
+heroTimeline.fromTo(".bgNoise", 
+  {r: "0%"},
+  {r: "100%", duration: 3, ease: "sine.inOut"}
+).fromTo(heading1.chars,
+  { opacity: 0, scale: 1.5, filter: "blur(10px)" },
+  { opacity: 1, scale: 1, filter: "blur(0px)", duration: 1.5, ease: "power3.out", stagger: 0.1}, "-=1.5"
+).fromTo(heading2.chars,
+  { opacity: 0, scale: 1.5, filter: "blur(10px)" },
+  { opacity: 1, scale: 1, filter: "blur(0px)", duration: 1.5, ease: "power3.out", stagger: 0.1}
+).fromTo(heading3.chars,
+  { opacity: 0, scale: 1.5, filter: "blur(10px)" },
+  { opacity: 1, scale: 1, filter: "blur(0px)", duration: 1.5, ease: "power3.out", stagger: 0.1}
+).fromTo(subtext.chars,
+  { opacity: 0 },
+  { opacity: 1, duration: 2, ease: "power1.out", stagger: 0.01}, "-=1.4"
+);
+
+// 3. Animate buttons
+const buttonWrappers = document.querySelectorAll(".primary-button-wrapper");
+const subtextEndTime = heroTimeline.duration();
+const button1Start = subtextEndTime - 1.2;
+const buttonDuration = 1.2;
+buttonWrappers.forEach((wrapper, i) => {
+  const button = wrapper.querySelector(".primary-button");
+  const inset = button.querySelector(".primary-button-inset");
+  const text = button.querySelector(".primary-button-text");
+
+  const timeline = gsap.timeline()
+    .fromTo(wrapper, { scale: 0 }, { scale: 1, duration: 0.9, ease: "quint.inOut" }, 0)
+    .fromTo(inset, { scale: 0 }, { scale: 1, duration: 0.9, ease: "quint.inOut" }, 0.1)
+    .fromTo(button, { width: "50px" }, { width: "170px", duration: 1, ease: "quint.inOut" }, 0.7)
+    .fromTo(text, { opacity: 0 }, { opacity: 1, duration: 0.2, ease: "ease" }, 1.3);
+
+  const start = i === 0 ? button1Start : button1Start + i * buttonDuration;
+  heroTimeline.add(timeline, start);
+});
