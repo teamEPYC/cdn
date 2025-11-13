3443f9c4abdadec2376daf3d829a590425a0fd3f
alevikx <131615202+alevikx@users.noreply.github.com>
2025-11-13 19:08:34 +0530

    Update prologue.js

--- kaviraj/v2/main/home/prologue.js
+++ kaviraj/v2/main/home/prologue.js
@@ -98,29 +98,29 @@ export function prologueAnimationMobile() {
 
 //Prologue Timeline Animation Sequence
 prologueTimeline.fromTo(prologue1text1.chars,
-    {filter: "blur(10px)", scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", scale: 1.5, opacity: 0},
     {filter: "blur(0px)", scale: 1, opacity: 1, stagger: 0.2, duration: 4, ease: "power1.out"}
 ).fromTo(prologue1text2.chars, 
-    {filter: "blur(10px)", scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", scale: 1.5, opacity: 0},
     {filter: "blur(0px)", scale: 1, opacity: 1, stagger: 0.2, duration: 4, ease: "power1.out"}, "+=2"
 ).to([prologue1text1.chars, prologue1text2.chars], 
-    {filter: "blur(10px)", scale: 1, opacity: 0, duration: 3, stagger: 0.1}, "+=4"
+    {filter: "blur(5px)", scale: 1, opacity: 0, duration: 3, stagger: 0.1}, "+=4"
 ).fromTo(prologue2text1.chars, 
-    {filter: "blur(10px)", scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", scale: 1.5, opacity: 0},
     {filter: "blur(0px)", scale: 1, opacity: 1, stagger: 0.2, duration: 4, ease: "power1.out"}
 ).fromTo(prologue2text2.chars, 
-    {filter: "blur(10px)", scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", scale: 1.5, opacity: 0},
     {filter: "blur(0px)", scale: 1, opacity: 1, stagger: 0.2, duration: 4, ease: "power1.out"}, "+=2"
 ).to([prologue2text1.chars, prologue2text2.chars], 
-    {filter: "blur(10px)", scale: 1, opacity: 0, duration: 3, stagger: 0.1}, "+=4"
+    {filter: "blur(5px)", scale: 1, opacity: 0, duration: 3, stagger: 0.1}, "+=4"
 ).fromTo(prologue3text1.chars, 
     {opacity: 0},
     {opacity: 1, stagger: 0.07, duration: 3, ease: "power1.out"}
 ).fromTo(prologue3text2.chars, 
-    {filter: "blur(10px)", y: -60, scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", y: -60, scale: 1.5, opacity: 0},
     {filter: "blur(0px)", y: 0, scale: 1, opacity: 1, stagger: 0.3, duration: 4, ease: "sine.inOut"}
 ).fromTo(prologue3text3.chars, 
-    {filter: "blur(10px)", y: -60, scale: 1.5, opacity: 0},
+    {filter: "blur(5px)", y: -60, scale: 1.5, opacity: 0},
     {filter: "blur(0px)", y: 0, scale: 1, opacity: 1, stagger: 0.3, duration: 4, ease: "sine.inOut"}, "<"
 ).to(".k-prologue-gi[data-sprite]", 
     {ease: "sine.inOut", duration: 15, onUpdate() {const el = this.targets()[0]; el._spriteSetProgress?.(this.progress());}}, "-=5"
@@ -130,11 +130,6 @@ prologueTimeline.fromTo(prologue1text1.chars,
 ).fromTo(prologueContents[2].querySelector(".k-prologue-text-wrapper"), 
     {filter: "brightness(100%)", scale: 1, opacity: 1, },
     {filter: "brightness(150%)", scale: 1.05, opacity: 0, duration: 5, ease: "sine.out"}, "+=3"
-)/*.fromTo(".k-prologue-gi", 
-    {height: "90%"},
-    {height: "75%", duration: 5, ease: "sine.inOut"}, "<-=1"
-);/*.to(prologue3image, 
-    {r: "50%", duration: 5, ease: "sine.inOut"}, "<-=2"
-);*/
+)
 
 }
