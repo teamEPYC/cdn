68f4f3d0ba320023af5761177eb2bbb3b5e7b221
Murtaza-TC1 <murtaza@epyc.in>
2026-07-23 11:02:03 +0530

    Update video sources for Safari and Chrome

--- polygon/polygon-revamp/pos/videomask.js
+++ polygon/polygon-revamp/pos/videomask.js
@@ -4,12 +4,12 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 trailsVideos.forEach((video) => {
   video.src = isSafari
-    ? "https://teamepyc.github.io/cdn/polygon/videos/polygon-revamp/Asset_Trails_Breakdown_SAFARI.mp4"
-    : "https://teamepyc.github.io/cdn/polygon/videos/polygon-revamp/Asset_Trails_Breakdown_CHROME.webm";
+    ? "https://polytech-assets.polygon.technology/videos/polygon-revamp/Asset_Trails_Breakdown_SAFARI.mp4"
+    : "https://polytech-assets.polygon.technology/videos/polygon-revamp/Asset_Trails_Breakdown_CHROME.webm";
 });
 
 if (isSafari) {
   const video = document.getElementById("pos-hero-video");
   video.src =
-    "https://teamepyc.github.io/cdn/polygon/videos/polygon-revamp/POS hero safari.mp4";
+    "https://polytech-assets.polygon.technology/videos/polygon-revamp/POS hero safari.mp4";
 }
